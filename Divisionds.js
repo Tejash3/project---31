9af24dc9e359a4d2ea43004434685e2626d94551
Tejash3 <76519367+Tejash3@users.noreply.github.com>
2021-02-24 18:41:08 +0530

    Add files via upload

---
+++ Divisionds.js
@@ -0,0 +1,20 @@
+class Divisons {
+    constructor(x,y,w,h){
+    var options = {
+        isStactic : true
+    }
+    this.body = Bodies.rectangle(x,y,w,h,options);
+    this.w = w;
+    this.h = h;
+    World.add(world,this.body);
+}
+display(){
+    var pos = this.body.position;
+    rectMode(CENTER)
+    fill("white");
+    rect(pos.x,pos.y,this.w,this.h)
+}
+}
+if (frameCount%60 === 0){
+    particales.push(new Particle(random(width/2-10,width / 2+10),10,10));    
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
